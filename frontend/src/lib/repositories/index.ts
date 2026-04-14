a74dd7209bff4a4b0fd879a5a0a00b0d7198087b
Alexey Donichev <alexeydonichev@gmail.com>
2026-04-14 04:07:48 +0000

    cleanup: удалён мёртвый код, секреты убраны из frontend, исправлены импорты

--- frontend/src/lib/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-export * as dashboardRepo from "./dashboard.repo";
-export * as salesRepo from "./sales.repo";
-export * as productsRepo from "./products.repo";
-export * as inventoryRepo from "./inventory.repo";
-export * as notificationsRepo from "./notifications.repo";
-export * as usersRepo from "./users.repo";
-export { rnpRepo } from "./rnp.repo";
